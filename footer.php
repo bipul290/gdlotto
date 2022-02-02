990210bf8f92a36e0414263952c980b5d96d986f
bipul290 <90503084+bipul290@users.noreply.github.com>
2022-02-02 20:33:41 +0600

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Grand Dragon</title>
+    <style>
+        h1, h2{
+            text-align: center;
+            margin-top: 50px;
+        }
+        h2{
+            border: 2px solid blueviolet;
+            padding: 20px 0;
+            font-size: 70px;
+            color: brown;
+            
+        }
+    </style>
+</head>
+<body>
+    <h1>GD-LOTTO (DRAGON)</h1>
+    <h2>1234</h2>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <!-- fontawesome kit -->
+     <script src="https://kit.fontawesome.com/f67243a4e0.js" crossorigin="anonymous"></script>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>my php</title>
+</head>
+<body>
+    <form action="login.php" method="POST">
+        <input class="form-control w-75 mt-5 mx-auto" type="text" name="stdname" placeholder="Enter Password"><br>
+        <!-- <input class="form-control w-75 mt-2 mx-auto" type="text" name="stdclass" placeholder="your Class"><br>
+        <input class="form-control w-75 mt-2 mx-auto" type="number" name="stdroll" placeholder="your Roll"><br> -->
+        <input class="form-control w-75 mt-2 bg-dark text-white mx-auto" type="submit" name="submit">
+    </form>
+ <!-- Bootstrap cdn -->
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
